06e8678b1b40fe0505aa30000e2e27ba1bfa623d
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-30 16:35:27 +0200

    Close app on back click

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,14 @@ import HomeContent from '@/components/habits/HomeContent';
 import VerifyUserMessage from '@/components/login/VerifyUserMessage';
 import { useAuth } from '@/context/AuthContext';
 import { useHabits } from '@/context/HabitsContext';
-import { useEffect } from 'react';
+import { useFocusEffect, useRouter } from 'expo-router';
+import { useCallback, useEffect } from 'react';
+import { BackHandler } from 'react-native';
 
 const HomeScreen = () => {
   const { user } = useAuth();
   const { fetchHabits } = useHabits();
+  const router = useRouter();
 
   const isVerified = user?.emailVerification;
 
@@ -16,6 +19,17 @@ const HomeScreen = () => {
       fetchHabits();
     }
   }, [user]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const onBackPress = () => {
+        BackHandler.exitApp();
+        return true;
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+    }, [router])
+  );
 
   return (
     <ProtectedRoute>
