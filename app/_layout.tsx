54a5daefe14cb3c09fe1b0a68826d6cb22a37d5d
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-05-31 19:56:29 +0200

    Update layout and hide splash screen after loading user

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from 'expo-router';
+import { useEffect, useState, useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import '../global.css';
 import { colorScheme } from 'nativewind';
-import { AuthProvider } from '@/context/AuthContext';
+import { AuthProvider, useAuth } from '@/context/AuthContext';
 import { HabitsProvider } from '@/context/HabitsContext';
 import { StreakProtectorProvider } from '@/context/StreakProtectorContext';
 import { MessageProvider } from '@/context/MessageContext';
@@ -9,30 +11,56 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SoundProvider } from '@/context/SoundProvider';
 
 colorScheme.set('light');
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+function Routes() {
+  const { isLoading: isAuthLoading } = useAuth();
+  const [isAppReady, SetIsAppReady] = useState(false);
+
+  useEffect(() => {
+    if (!isAuthLoading) {
+      SetIsAppReady(true);
+    }
+  }, [isAuthLoading]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isAppReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isAppReady]);
+
+  if (!isAppReady) return null;
+
+  return (
+    <SafeAreaProvider onLayout={onLayoutRootView}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="habit/[id]" />
+        <Stack.Screen name="verify-email" />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
 
 const RootLayout = () => {
   return (
-    <SafeAreaProvider>
-      <AuthProvider>
-        <SoundProvider>
-          <MessageProvider>
-            <HabitsProvider>
-              <StreakProtectorProvider>
-                <Stack
-                  screenOptions={{
-                    headerShown: false,
-                  }}>
-                  <Stack.Screen name="index" />
-                  <Stack.Screen name="login" />
-                  <Stack.Screen name="habit/[id]" />
-                  <Stack.Screen name="verify-email" />
-                </Stack>
-              </StreakProtectorProvider>
-            </HabitsProvider>
-          </MessageProvider>
-        </SoundProvider>
-      </AuthProvider>
-    </SafeAreaProvider>
+    <AuthProvider>
+      <SoundProvider>
+        <MessageProvider>
+          <HabitsProvider>
+            <StreakProtectorProvider>
+              <Routes />
+            </StreakProtectorProvider>
+          </HabitsProvider>
+        </MessageProvider>
+      </SoundProvider>
+    </AuthProvider>
   );
 };
 
